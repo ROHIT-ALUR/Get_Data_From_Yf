ee3f35bb974fab123a1ac1f560db3c13413dfe09
ROHIT-ALUR <rohitalur2k3@gmail.com>
2026-04-24 19:49:11 +0530

    Update rohhbot.py

--- rohhbot.py
+++ rohhbot.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1MAy2WUmpxDYjgaiiPWT1b91iee2RHyzW
 """
 
-pip install extra-streamlit-components
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
